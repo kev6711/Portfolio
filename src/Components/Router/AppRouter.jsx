4832324489ef4d83e37e2dbeec0a549678518d74
kev6711 <kevin.heckenmeyer@gmail.com>
2025-12-04 12:31:20 +0100

    Ajout du composant Footer

--- src/Components/Router/AppRouter.jsx
+++ src/Components/Router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import Home from "../../pages/Home.jsx"
 import Projet from "../../pages/Projet.jsx"
 import Header from "../Header/Header.jsx"
+import Footer from "../Footer/Footer.jsx"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function AppRouter() {
@@ -11,6 +12,7 @@ function AppRouter() {
                 <Route path="/" element={<Home />} />
                 <Route path="/projet/:id" element={<Projet />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     )
 }
