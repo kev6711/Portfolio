b801ad96462b449e5fb1614f11d98621943cc95b
kev6711 <kevin.heckenmeyer@gmail.com>
2025-12-08 11:19:58 +0100

    Ajout du composant A-propos + Ajustements styles du Hero et du Footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import A_propos from "../Components/A_propos/A_propos.jsx"
 import Hero from "../Components/Hero/Hero.jsx"
 
 function Home() {
     return (
         <main>
             <Hero />
+            <A_propos />
         </main>
     )
 }
